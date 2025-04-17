2d156a7765da3a8794bbfb649b5424be4f718d0b
Jacques <173047302+Jacques6776@users.noreply.github.com>
2025-04-17 17:12:33 +0200

    Difficulty factors added with tut

--- Assets/Scripts/ObstacleSpawnController.cs
+++ Assets/Scripts/ObstacleSpawnController.cs
@@ -4,22 +4,37 @@ using UnityEngine.VFX;
 public class ObstacleSpawnController : MonoBehaviour
 {
     //code referenced from Muddy Wolf https://www.youtube.com/watch?v=vClEQ1GqMPw&list=PLfX6C2dxVyLylMufxTi7DM9Vjlw5bff1c&index=3
+    //Sets up spawning and time between spawns
     [SerializeField] private GameObject[] obstaclePrefabs;
     [SerializeField] private Transform obstacleParent;
     public float obstacleSpawnTime = 2f;
     public float obstacleSpeed = 1f;
+
+    //Setting up difficulty curve
+    [Range(0, 1)] public float obstacleSpawnTimeFactor = 0.1f;
+    [Range(0, 1)] public float obstacleSpeedFactor = 0.2f;
+
+    private float currentObstacleSpawnTime;
+    private float currentObstacleSpeed;
+
+    private float timeAlive; //record how long the player has been alive
 
     private float timeUntilObstacleSpawn;
 
     private void Start()
     {
         LevelManager.Instance.onGameOver.AddListener(ClearObstacles);
+        LevelManager.Instance.onPlay.AddListener(ResetFactors);
     }
 
     private void Update()
     {
         if (LevelManager.Instance.isPlaying)
         {
+            timeAlive += Time.deltaTime;
+
+            CalculateFactors();
+
             ObstacleSpawnLoop();
         }        
     }
@@ -28,14 +43,14 @@ public class ObstacleSpawnController : MonoBehaviour
     {
         timeUntilObstacleSpawn += Time.deltaTime;
 
-        if (timeUntilObstacleSpawn >= obstacleSpawnTime)
+        if (timeUntilObstacleSpawn >= currentObstacleSpawnTime)
         {
             ObstacleSpawn();
             timeUntilObstacleSpawn = 0f;
         }
     }
 
-    private void ClearObstacles()
+    private void ClearObstacles() //clears all obstacles on death
     {
         foreach(Transform child in obstacleParent)
         {
@@ -43,7 +58,20 @@ public class ObstacleSpawnController : MonoBehaviour
         }
     }
 
-    private void ObstacleSpawn()
+    private void CalculateFactors() // ups the difficulty by an equation
+    {
+        currentObstacleSpawnTime = obstacleSpawnTime / Mathf.Pow(timeAlive, obstacleSpawnTimeFactor);
+        currentObstacleSpeed = obstacleSpeed * Mathf.Pow(timeAlive, obstacleSpeedFactor);
+    }
+
+    private void ResetFactors() // resets the game to its base factors upon game over / restart
+    {
+        timeAlive = 1f;
+        currentObstacleSpawnTime = obstacleSpawnTime;
+        currentObstacleSpeed = obstacleSpeed;
+    }
+
+    private void ObstacleSpawn()//Spawns obstacles
     {
         GameObject obstacleToSpawn = obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)];
 
@@ -51,6 +79,6 @@ public class ObstacleSpawnController : MonoBehaviour
         spawnedObstacle.transform.parent = obstacleParent;
 
         Rigidbody2D obstacleRB = spawnedObstacle.GetComponent<Rigidbody2D>();
-        obstacleRB.linearVelocity = Vector2.left * obstacleSpeed;
+        obstacleRB.linearVelocity = Vector2.left * currentObstacleSpeed;
     }
 }
